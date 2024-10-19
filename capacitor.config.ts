df7b381acfb6d08c21b4ecae511ea60c4e47c63e
RayenRk <rakkadrayen@gmail.com>
2024-10-19 13:47:55 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'quizz-app',
+  webDir: 'www'
+};
+
+export default config;
